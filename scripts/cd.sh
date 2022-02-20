d87195b8bf81da417e1d7c6c22d8f310e0a40497
Martín Montes <martin11lrx@gmail.com>
2022-02-20 20:55:07 +0100

    Updated deploy script

--- scripts/cd.sh
+++ scripts/cd.sh
@@ -5,12 +5,10 @@ set -e
 repo="bankroach"
 release="bankroach"
 chart="mmontes11/bankroach"
-
-git fetch --all
-tag=$(git describe --tags $(git rev-list --tags --max-count=1))
+namespace="bankroach"
 
 helm repo add "$repo" https://charts.mmontes-dev.duckdns.org
 helm repo update
 
 echo "🚀 Deploying '$chart' with image version '$tag'..."
-helm upgrade --install "$release" "$chart" --set image.tag=$tag
+helm upgrade --install "$release" "$chart" --namespace "$namespace"
